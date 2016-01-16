8c498eab71285edb628b7d71031d9a16428624b0
Wiseluster <Wiseluster@users.noreply.github.com>
2016-01-16 15:34:24 +0800

    Update install_prerequisites.sh

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -4,11 +4,11 @@ PREREQ_DATA=`pwd`
 
 if [ "${UID}" -ne 0 ];
 then
-	echo "${0}: error: root privillage repquired." 1>&2
+	echo "${0}: error: root privillege repquired." 1>&2
 	exit 1
 fi
 
-dpkg -i bochs.deb && \
+dpkg -i com.isoftru.bochs-1.0.0-1.deb && \
 cd / && \
 tar xvf ${PREREQ_DATA}/binutils-x86_64-linux-gnu-2.25.1.tar.bz2
 
